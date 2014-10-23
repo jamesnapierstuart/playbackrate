072da0aa93c1e4c994d79b994bf236c74b7b2cfb
jamesnapierstuart <jamiestuart@jamies-mbp.home>
2014-10-23 21:46:17 +0100

    It worked before so reverting back

--- sketch.js
+++ sketch.js
@@ -2,46 +2,15 @@ var song
 
 function preload() {
   // Load a sound file
-  song = loadSound('snare.wav');
+  song = loadSound('b.m4a');
 }
 
 function setup() {
   createCanvas( displayWidth, displayHeight )
-  background(255);
+  background(0)
   song.play()
 }
 
 function draw() {
-
-// var speed = map(mouseY, 0.1, height, -20, 20)
-// var size = 12000/speed
-
-
-  // speed = constrain(speed, 0.01, 4);
-  // song.rate(speed)
-  // song.play()
-
-
-  // Inverse variation
-
-
-  b = color(0, 0, 200, 40)
-  p = color(200, 0, 100, 40)
-  g = color(0, 200, 0, 40)
-  f = new Array(3)
-  f[0] = b
-  f[1] = p
-  f[2] = g
-  l = f[int(random(3))]
-  fill(l)
-  noStroke()
-  ellipse(displayWidth/2, random(displayHeight), random(size*0.01), random(size*0.01))
-  ellipse(displayWidth/2+displayWidth/2.5, random(displayHeight), random(size*0.01), random(size*0.01))
-  ellipse(displayWidth/2-displayWidth/2.5, random(displayHeight), random(size*0.01), random(size*0.01))
-  ellipse(displayWidth/2+displayWidth/4, random(displayHeight), random(size*0.01), random(size*0.01))
-  ellipse(displayWidth/2-displayWidth/4, random(displayHeight), random(size*0.01), random(size*0.01))
-  ellipse(displayWidth/2+displayWidth/8, random(displayHeight), random(size*0.01), random(size*0.01))
-  ellipse(displayWidth/2-displayWidth/8, random(displayHeight), random(size*0.01), random(size*0.01))
-
-
+  ellipse(20, 20, 20, 20)
 }

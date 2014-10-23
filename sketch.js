dc0abdd22161f0ac0c05d265bc7dea81ea271b21
jamesnapierstuart <jamiestuart@jamies-mbp.home>
2014-10-23 22:36:50 +0100

    ellispe for test

--- sketch.js
+++ sketch.js
@@ -14,6 +14,7 @@ function draw() {
   ellipse(20, 20, 20, 20)
   ellipse(width/2, height/2, 20, 20)
   ellipse(width/2+20, height/2+20, 20, 20)
+  ellipse(width/2+200, height/2+20, 20, 20)
 }
 
 function doOnOrientationChange()

7a5e47906dc5f4bcaaa29145bebf99d5381bc15f
jamesnapierstuart <jamiestuart@jamies-mbp.home>
2014-10-23 22:01:30 +0100

    Added an ellipse as confirmation

--- sketch.js
+++ sketch.js
@@ -9,12 +9,13 @@ function setup() {
   createCanvas( displayWidth, displayHeight )
   background(0)
   if (song.isLoaded()) {
-    env.triggerAttack(song);
-    touchStarted = true;
+    env.triggerAttack(song)
+    touchStarted = true
   }
   // song.play()
 }
 
 function draw() {
   ellipse(20, 20, 20, 20)
+  ellipse(width/2, height/2, 20, 20)
 }

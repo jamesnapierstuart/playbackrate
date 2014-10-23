f32ee4eb0d16af8c53ea5be738442df6f344ef89
jamesnapierstuart <jamiestuart@jamies-mbp.home>
2014-10-23 22:28:03 +0100

    Changed it

--- sketch.js
+++ sketch.js
@@ -1,21 +1,17 @@
 var song
 
 function preload() {
-  // Load a sound file
   song = loadSound('b.m4a')
-  // env.triggerAttack(song)
-  // touchStarted = true
 }
 
 function setup() {
   createCanvas( displayWidth, displayHeight )
   background(0)
-  window.addEventListener('click', function() {
-    song.play()
-  }, true)
+  song.play()
 }
 
 function draw() {
   ellipse(20, 20, 20, 20)
   ellipse(width/2, height/2, 20, 20)
+  ellipse(width/2+20, height/2+20, 20, 20)
 }
